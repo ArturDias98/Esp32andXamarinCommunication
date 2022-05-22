3bb92c6f094998fbe9e02f5da015c93ab4b555d7
Artur <arturdrm@hotmail.com>
2022-05-22 10:56:14 -0300

    CHANGED: Sending data over bluetooth with 50ms time interval

--- Firmware/src/main.cpp
+++ Firmware/src/main.cpp
@@ -1,9 +1,13 @@
 #include <Arduino.h>
 #include <BluetoothSerial.h>
 
+#define SERIAL_TIME_INTERVAL 1000 //miliseconds
+#define BT_TIME_INTERVAL 50 //miliseconds
+
 BluetoothSerial serialBT;
 uint8_t dataReceived;
-unsigned long plotTime;
+unsigned long serialSendInterval;
+unsigned long btSendInterval;
 const int analogPin = 34;
 int analogData = 0;
 
@@ -12,11 +16,12 @@ void SendData_Serial();
 void setup()
 {
   // put your setup code here, to run once:
-  Serial.begin(115200);
+  Serial.begin(9600);
   serialBT.begin("ESP32");
   Serial.println("Started");
   dataReceived = 0;
-  plotTime = millis();
+  serialSendInterval = millis();
+  btSendInterval = millis();
 }
 
 void loop()
@@ -27,30 +32,34 @@ void loop()
 
   SendData_Bluetooth();
   SendData_Serial();
-
 }
 
 void SendData_Bluetooth()
 {
   // Sending 16 bits of data over bluetooth.
-  uint8_t data1 = analogData & 0xFF;//lsb
-  uint8_t data2 = (analogData >> 8) & 0xFF;//msb
+  uint8_t data1 = analogData & 0xFF;        // lsb
+  uint8_t data2 = (analogData >> 8) & 0xFF; // msb
 
-  serialBT.write(0xAB);//Protocol header
-  serialBT.write(0xCD);// Protocol header
-  serialBT.write(data1);//Data lsb
-  serialBT.write(data2);//Data msb
-  serialBT.write(0xAF);//Protocol tail
-  serialBT.write(0xCF);//Protocol tail
+  if (millis() - btSendInterval >= BT_TIME_INTERVAL)
+  {
+    serialBT.write(0xAB);  // Protocol header
+    serialBT.write(0xCD);  // Protocol header
+    serialBT.write(data1); // Data lsb
+    serialBT.write(data2); // Data msb
+    serialBT.write(0xAF);  // Protocol tail
+    serialBT.write(0xCF);  // Protocol tail
+
+    btSendInterval = millis();
+  }
 }
 
 void SendData_Serial()
 {
-  //Send data over serial with one second interval.
-  //Just for debug.
-  if (millis() - plotTime >= 1000)
+  // Send data over serial with one second interval.
+  // Just for debug.
+  if (millis() - serialSendInterval >= SERIAL_TIME_INTERVAL)
   {
-    plotTime = millis();
+    serialSendInterval = millis();
     Serial.println(analogData);
   }
 }
